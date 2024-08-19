23ed112dea8d9652590c5400f5d2e1683d34aa80
naingwinphyoe <naingwinphyoe.19@gmail.com>
2024-08-19 16:31:41 +0630

    'Todo-Sample'

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from '../components/Todo.js';
+
+export default function TodoList({todos, deleteTodo}) {
+  return (
+    <ul className="todo-list">
+          
+          {todos.map(todo => (
+            <Todo todo={todo} key={todo.id} deleteTodo={deleteTodo}/>
+
+          ))}
+          
+    </ul>
+  )
+}
